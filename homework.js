8b172cf714224cd2634947693d008d52efee6b11
sparkmahadi <mahadi.development@gmail.com>
2022-08-10 19:51:43 +0600

    added homework

---
+++ homework.js
@@ -0,0 +1,21 @@
+// Homework: 1
+// write a function that will take 3 numbers will return the max number
+
+/* function maxFinder(a, b, c){
+    let maxNumber = Math.max(a,b,c);
+    console.log(maxNumber);
+}
+
+maxFinder(1,2,3); */
+
+
+
+// Homework: 2
+// Write a function that will take 3 parameters and will return the min number
+
+function maxFinder(a, b, c){
+    let maxNumber = Math.min(a,b,c);
+    console.log(maxNumber);
+}
+
+maxFinder(1,2,3); 
