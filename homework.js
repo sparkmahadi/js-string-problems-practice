4ddd917c3cdec7679f446006869286d95f7f0be3
sparkmahadi <mahadi.development@gmail.com>
2022-08-10 20:48:47 +0600

    updated homework file

--- homework.js
+++ homework.js
@@ -13,9 +13,28 @@ maxFinder(1,2,3); */
 // Homework: 2
 // Write a function that will take 3 parameters and will return the min number
 
-function maxFinder(a, b, c){
+/* function maxFinder(a, b, c){
     let maxNumber = Math.min(a,b,c);
     console.log(maxNumber);
 }
 
-maxFinder(1,2,3); 
+maxFinder(1,2,3);  */
+
+
+// Homework: Write a function to get the lowest number in an array
+function minInArray(numbers){
+        let smallest = numbers[0];
+
+    for(let i=0; i<numbers.length; i++){
+        const index = i;
+        const element = numbers[index];
+        if(element<smallest){
+            smallest = element;
+        }
+    }
+    return smallest;
+}
+
+const heights = [167, 190, 120, 165, 137, 265];
+const smallest = minInArray(heights);
+console.log('smallest person is: ', smallest);
